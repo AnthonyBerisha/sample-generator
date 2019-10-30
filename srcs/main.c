b4e76f49373b98761a751d160cc146c8e56156f3
anthony <berishaanthony@gmail.com>
2019-10-30 21:32:31 +0100

    pushhh

--- srcs/main.c
+++ srcs/main.c
@@ -1,132 +1,42 @@
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include "avformat.h"
-// #include "error.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "libavformat/avformat.h"
+#include "error.h"
 
-// void	check_error(int errcode)
-// {
-// 	char errbuff[512] = {};
-// 	if (!av_strerror(errcode, errbuff, 512) && errcode != 0)
-// 		printf("**********\nError %d\n%s\n**********\n", errcode, errbuff);
-// 	else return;
-// }
-
-// int 	main(int ac, char **av)
-// {
-
-// 	// /home/anthony/Documents/projects/sample-generator
-// 	const char *url = NULL;
-// 	AVFormatContext *s = NULL;
-// 	s = avformat_alloc_context();
-// 	int ret = 0;
-// 	if (ac == 2)
-// 	{
-// 		url = av[1];
-// 		printf("Opening %s\n", url);
-// 	}	
-// 	else
-// 		return (1);
-	
-// 	if ((ret = avformat_open_input(&s, url, NULL, NULL)) < 0)
-// 	{
-// 		check_error(ret);
-// 		printf("Failed to open %s\nAborting\n", url);
-// 		abort();
-// 	}
-// 	else
-// 	{
-// 		printf("opened %s\n", url);
-// 	}
-	
-	
-	
-
-	
-// 	return(0);
-// }
-
-#include <libavcodec/avcodec.h>
-#include <libavformat/avformat.h>
-#include <libavformat/avio.h>
-#include <libavutil/file.h>
-struct buffer_data {
-    uint8_t *ptr;
-    size_t size; ///< size left in the buffer
-};
-static int read_packet(void *opaque, uint8_t *buf, int buf_size)
+void	check_error(int errcode)
 {
-    struct buffer_data *bd = (struct buffer_data *)opaque;
-    buf_size = FFMIN(buf_size, bd->size);
-    printf("ptr:%p size:%zu\n", bd->ptr, bd->size);
-    /* copy internal buffer data to buf */
-    memcpy(buf, bd->ptr, buf_size);
-    bd->ptr  += buf_size;
-    bd->size -= buf_size;
-    return buf_size;
+	char errbuff[512] = {};
+	if (!av_strerror(errcode, errbuff, 512) && errcode != 0)
+		printf("**********\nError %d\n%s\n**********\n", errcode, errbuff);
+	else return;
 }
-int main(int argc, char *argv[])
+
+int 	main(int ac, char **av)
 {
-    AVFormatContext *fmt_ctx = NULL;
-    AVIOContext *avio_ctx = NULL;
-    uint8_t *buffer = NULL, *avio_ctx_buffer = NULL;
-    size_t buffer_size, avio_ctx_buffer_size = 4096;
-    char *input_filename = NULL;
-    int ret = 0;
-    struct buffer_data bd = { 0 };
-    if (argc != 2) {
-        fprintf(stderr, "usage: %s input_file\n"
-                "API example program to show how to read from a custom buffer "
-                "accessed through AVIOContext.\n", argv[0]);
-        return 1;
-    }
-    input_filename = argv[1];
-    /* register codecs and formats and other lavf/lavc components*/
-    av_register_all();
-    /* slurp file content into buffer */
-    ret = av_file_map(input_filename, &buffer, &buffer_size, 0, NULL);
-    if (ret < 0)
-        goto end;
-    /* fill opaque structure used by the AVIOContext read callback */
-    bd.ptr  = buffer;
-    bd.size = buffer_size;
-    if (!(fmt_ctx = avformat_alloc_context())) {
-        ret = AVERROR(ENOMEM);
-        goto end;
-    }
-    avio_ctx_buffer = av_malloc(avio_ctx_buffer_size);
-    if (!avio_ctx_buffer) {
-        ret = AVERROR(ENOMEM);
-        goto end;
-    }
-    avio_ctx = avio_alloc_context(avio_ctx_buffer, avio_ctx_buffer_size,
-                                  0, &bd, &read_packet, NULL, NULL);
-    if (!avio_ctx) {
-        ret = AVERROR(ENOMEM);
-        goto end;
-    }
-    fmt_ctx->pb = avio_ctx;
-    ret = avformat_open_input(&fmt_ctx, NULL, NULL, NULL);
-    if (ret < 0) {
-        fprintf(stderr, "Could not open input\n");
-        goto end;
-    }
-    ret = avformat_find_stream_info(fmt_ctx, NULL);
-    if (ret < 0) {
-        fprintf(stderr, "Could not find stream information\n");
-        goto end;
-    }
-    av_dump_format(fmt_ctx, 0, input_filename, 0);
-end:
-    avformat_close_input(&fmt_ctx);
-    /* note: the internal buffer could have changed, and be != avio_ctx_buffer */
-    if (avio_ctx) {
-        av_freep(&avio_ctx->buffer);
-        av_freep(&avio_ctx);
-    }
-    av_file_unmap(buffer, buffer_size);
-    if (ret < 0) {
-        fprintf(stderr, "Error occurred: %s\n", av_err2str(ret));
-        return 1;
-    }
-    return 0;
+
+	// /home/anthony/Documents/projects/sample-generator
+	const char *url = NULL;
+	AVFormatContext *s = NULL;
+	s = avformat_alloc_context();
+	int ret = 0;
+	if (ac == 2)
+	{
+		url = av[1];
+		printf("Opening %s\n", url);
+	}	
+	else
+		return (1);
+	
+	if ((ret = avformat_open_input(&s, url, NULL, NULL)) < 0)
+	{
+		check_error(ret);
+		printf("Failed to open %s\nAborting\n", url);
+		abort();
+	}
+	else
+	{
+		printf("opened %s\n", url);
+	}
+	return(0);
 }
+
